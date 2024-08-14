67bb47e7a95dcebaba15706c2ebe1a054b97ec39
Zidan leo permana <zidanleopermana012@gmail.com>
2024-08-14 15:36:47 +0700

    Complete the lesson
    this function will also delete duplicated elements from the
    initial array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    const arr = [].concat([...new Set(arguments[0])]);
+    for (let i = arguments.length - 1; i > 0; i--) {
+        let valueToRemove = arguments[i];
+        let index = arr.indexOf(valueToRemove);
+        if (index !== -1) {
+            arr.splice(index, 1)
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
