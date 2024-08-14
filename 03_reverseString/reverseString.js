5a1e7911c9ba62f46c1ba2041660db8b93b7ee4f
Zidan leo permana <zidanleopermana012@gmail.com>
2024-08-14 13:52:52 +0700

    Finishing the lesson

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(words) {
+    result = '';
+    for (let i = words.length; i > 0; i--) {
+        result += words[i - 1];
+    }
 
+    return result;
 };
 
 // Do not edit below this line
