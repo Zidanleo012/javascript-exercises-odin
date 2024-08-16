d66427884e0e204015e697bd4350c8937bc15f72
Zidan leo permana <zidanleopermana012@gmail.com>
2024-08-16 16:29:44 +0700

    Finish the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '')
 
+    let reversedStr = str.split('').reverse().join('');
+
+    return reversedStr === str
 };
 
 // Do not edit below this line
