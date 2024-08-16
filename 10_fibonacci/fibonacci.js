a7827f8ea6013189b59ac32b07137401c138ae7d
Zidan leo permana <zidanleopermana012@gmail.com>
2024-08-16 18:30:02 +0700

    finishing the lesson

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num === 0) {
+        return 0;
+    } else if (num < 0) {
+        return 'OOPS'
+    }
 
+    let num1 = 0;
+    let num2 = 1;
+    let temp = 0;
+
+    for (let i = 0; i < num; i++) {
+        temp = num2 + num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    return num1;
 };
 
 // Do not edit below this line
