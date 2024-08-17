2734d3b3a26dff3dc06ebacbdb2ca3d13da1846b
Zidan leo permana <zidanleopermana012@gmail.com>
2024-08-17 21:48:31 +0700

    Finish the exercise
    Phew, finally!!!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const YEAR = new Date().getFullYear();
 
+    let oldestPeople = people.reduce((curr, people) => {
+        const currAge = (curr.yearOfDeath || YEAR) - curr.yearOfBirth;
+        const peopleAge = (people.yearOfDeath || YEAR) - people.yearOfBirth;
+
+        return currAge > peopleAge ? curr : people
+    })
+
+    return oldestPeople;
 };
 
 // Do not edit below this line
